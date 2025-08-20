dbb3faf404c9167ba9a390237bca5b05f6065bb6
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-08-20 10:10:10 +0530

    [jayesh tapdiya] : modify the user route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import * as userController from '../controllers/user.controller';
 import { newUserValidator } from '../validators/user.validator';
+import { userAuth } from '../middlewares/auth.middleware';
+import { resetpasswordvalidtor } from '../validators/resetpassword.validator';
 
 
 dotenv.config();
@@ -15,7 +17,7 @@ router.post('/login', userController.login);
 
 router.post('/forget_password', userController.forgetpassword);
 
-router.post('/reset_password',)
+router.post('/reset_password', userAuth(process.env.jwt_sceret_key), resetpasswordvalidtor, userController.resetpassword)
 
 
 export default router;
