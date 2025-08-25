89eb786a6ed3088d53cf777c2ed0fd5017c92e31
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-08-25 16:07:41 +0530

    [jayesh tapdiya] added the service for: addnote, archived note, trash note, color note,update note ,delete note

--- src/services/notes.service.js
+++ src/services/notes.service.js
@@ -33,3 +33,174 @@ export const getallnotes = async (createdBy, email) => {
 }
 
 
+export const addnote = async (body) => {
+    try {
+
+        const data = await Notes.create(body);
+        return {
+            code: 200,
+            message: "sccuseefully get all the notes",
+            data: data,
+            success: true
+        }
+
+    } catch (error) {
+        return {
+            code: 500,
+            message: "Internal server error",
+            error: error.message,
+            success: false,
+        };
+    }
+}
+
+
+export const archived = async (body, id) => {
+    try {
+        const data = await Notes.findOne({ where: { createdBy: body.createdBy, note_id: id } });
+        if (!data) {
+            return {
+                code: 400,
+                message: "note did not exisit",
+                success: false
+            }
+        } else {
+            data.isArchived = !data.isArchived;
+            await data.save();
+            return {
+                code: 200,
+                message: `Note has been ${data.isArchived ? 'archived' : 'unarchived'} successfully`,
+                data: data,
+                success: true
+            }
+        }
+
+    } catch (error) {
+        return {
+            code: 500,
+            message: "Internal server error",
+            error: error.message,
+            success: false,
+        };
+    }
+}
+
+
+export const trash = async (body, id) => {
+    try {
+        const data = await Notes.findOne({ where: { createdBy: body.createdBy, note_id: id } });
+        if (!data) {
+            return {
+                code: 400,
+                success: false,
+                message: "note doest not exisit"
+            }
+        } else {
+            data.isDeleted = !data.isDeleted;
+            await data.save();
+            return {
+                code: 200,
+                message: `Note has been ${data.isDeleted ? 'trash' : 'trash'} successfully`,
+                data: data,
+                success: true
+            }
+        }
+    } catch (error) {
+        return {
+            code: 500,
+            message: "Internal server error",
+            error: error.message,
+            success: false,
+        };
+    }
+}
+
+
+export const update = async (body, id) => {
+    try {
+
+        const data = await Notes.findOne({ where: { createdBy: body.createdBy, note_id: id } });
+        if (!data) {
+            return {
+                code: 400,
+                success: false,
+                message: "note doest not exisit"
+            }
+        } else {
+            await data.update({ ...body });
+            return {
+                code: 200,
+                message: `Note has been updated successfully`,
+                data: data,
+                success: true
+            }
+        }
+
+    } catch (error) {
+        return {
+            code: 500,
+            message: "Internal server error",
+            error: error.message,
+            success: false,
+        };
+    }
+}
+
+
+export const deletenote = async (body, id) => {
+    try {
+        const data = await Notes.findOne({ where: { createdBy: body.createdBy, note_id: id } });
+
+        if (!data) {
+            return {
+                code: 400,
+                success: false,
+                message: "note doest not exisit"
+            }
+        } else {
+            await data.destroy();
+            return {
+                code: 200,
+                message: `Note has been deleted successfully`,
+                success: true
+            }
+        }
+
+    } catch (error) {
+        return {
+            code: 500,
+            message: "Internal server error",
+            error: error.message,
+            success: false,
+        }
+    }
+}
+
+export const notecolor = async (body, id) => {
+    try {
+        const data = await Notes.findOne({ where: { createdBy: body.createdBy, note_id: id } });
+        if (!data) {
+            return {
+                code: 400,
+                message: "the note is not found",
+                color: body.color,
+                success: false
+            }
+        } else {
+            await data.update({ color: body.color });
+            return {
+                code: 200,
+                message: `Note  color has been updated successfully`,
+                color: body.color,
+                success: true
+            }
+        }
+    } catch (error) {
+        return {
+            code: 500,
+            message: "Internal server error",
+            error: error.message,
+            success: false,
+        }
+    }
+}
