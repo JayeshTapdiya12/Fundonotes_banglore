7d6df4acd1403a287515cad35bda6ff8e660afb2
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-08-25 16:06:44 +0530

    [jayesh tapdiya] added the routing for: addnote, archived note, trash note, color note,update note ,delete note

--- src/routes/notes.route.js
+++ src/routes/notes.route.js
@@ -10,6 +10,18 @@ const router = express.Router();
 
 router.get('/getallnotes', userAuth(process.env.jwt_sceret_key), notesController.getallnotes);
 
+router.post('/addnote', userAuth(process.env.jwt_sceret_key), notesController.addnote);
+
+router.post('/:_id/archived', userAuth(process.env.jwt_sceret_key), notesController.archived);
+
+router.post('/:_id/trash', userAuth(process.env.jwt_sceret_key), notesController.trash);
+
+router.put('/:_id', userAuth(process.env.jwt_sceret_key), notesController.updatenote);
+
+router.patch('/:_id/color', userAuth(process.env.jwt_sceret_key), notesController.notecolor);
+
+router.delete('/:_id', userAuth(process.env.jwt_sceret_key), notesController.deletenote);
+
 
 
 export default router;
