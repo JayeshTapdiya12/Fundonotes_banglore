04876a7d5611f06b7d62c86b3d098b4f54179c20
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-08-25 16:07:26 +0530

    [jayesh tapdiya] added the controller for: addnote, archived note, trash note, color note,update note ,delete note

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -10,3 +10,60 @@ export const getallnotes = async (req, res, next) => {
         res.status(data.code).json(data);
     }
 }
+
+
+export const addnote = async (req, res, next) => {
+    try {
+        const data = await notesService.addnote(req.body);
+        res.status(data.code).json(data);
+    } catch (error) {
+        res.status(data.code).json(data);
+    }
+}
+
+export const archived = async (req, res, next) => {
+    try {
+        const data = await notesService.archived(req.body, req.params._id);
+        res.status(data.code).json(data);
+    } catch (error) {
+        res.status(data.code).json(data);
+    }
+}
+
+export const trash = async (req, res, next) => {
+    try {
+        const data = await notesService.trash(req.body, req.params._id);
+        res.status(data.code).json(data);
+    } catch (error) {
+        res.status(data.code).json(data);
+
+    }
+}
+
+export const updatenote = async (req, res, next) => {
+    try {
+        const data = await notesService.update(req.body, req.params._id);
+        res.status(data.code).json(data);
+    } catch (error) {
+        res.status(data.code).json(data);
+
+    }
+}
+
+export const deletenote = async (req, res, next) => {
+    try {
+        const data = await notesService.deletenote(req.body, req.params._id);
+        res.status(data.code).json(data)
+    } catch (error) {
+        res.status(data.code).json(data);
+    }
+}
+
+export const notecolor = async (req, res, next) => {
+    try {
+        const data = await notesService.notecolor(req.body, req.params._id);
+        res.status(data.code).json(data);
+    } catch (error) {
+        res.status(data.code).json(data);
+    }
+}
