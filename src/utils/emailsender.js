13f17aff642ac7954137f4b770b34ac17029fb59
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-08-28 12:21:29 +0530

    [jayesh tapdiya] Updated email sender functionality: Now any component can send emails.

--- src/utils/emailsender.js
+++ src/utils/emailsender.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 
 require('dotenv').config();
 
-export const sendMail = async (email, token) => {
+export const sendMail = async (email, subject, body) => {
 
     console.log("EMAIL from env:", process.env.EMAIL_USER);
     console.log("PASSWORD from env:", process.env.PASSWORD_USER ? '✅ Present' : '❌ MISSING');
@@ -20,14 +20,8 @@ export const sendMail = async (email, token) => {
         const info = await transporter.sendMail({
             from: process.env.EMAIL_USER,
             to: email,
-            subject: 'Password Reset Link',
-            html: `
-                <h1>Hello,</h1>
-                <p>Click the link below to reset your password:</p>
-                <a href="http://localhost:${process.env.APP_PORT}/api/${process.env.API_VERSION}/users/forget_password/${token}">
-                    Reset Password
-                </a>
-            `,
+            subject: subject,
+            html: body,
         });
 
         console.log("Message sent: " + info.messageId);
