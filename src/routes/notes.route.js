e1281ad0ff5ced993c1c4f0e0b378e10c62777e7
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-08-29 10:47:23 +0530

    [jayesh tapdiya]updated the routing for  the reminder and added the functionalty of get reminder, add reminder, delete reminder

--- src/routes/notes.route.js
+++ src/routes/notes.route.js
@@ -36,16 +36,39 @@ router.put('/:_id/updatelabel', userAuth(process.env.jwt_sceret_key), notesContr
 
 
 // delete the label
-router.post('/:_id/deletelabel', userAuth(process.env.jwt_sceret_key), notesController.deletelabel);
+router.delete('/:_id/deletelabel', userAuth(process.env.jwt_sceret_key), notesController.deletelabel);
 
 
-
-
+// {
+//   "date": "2025-08-29",
+//   "time": "14:30:00",
+//   "repeat": "weekly",
+//   "repeat_custom": {
+//     "daysOfWeek": [1, 3, 5],
+//     "interval": 2,
+//     "endDate": "2025-12-31"
+//   }
+// }
 
 
 // reminder
 // add remainder - date, time, repeat-no,yes-daily,weekly,monthly,yearly,customize
 // delete remainder
+
+// get the reminder
+router.get('/:_id/reminder', userAuth(process.env.jwt_sceret_key), notesController.getreminder);
+
+// add the reminder
+router.post('/:_id/reminder', userAuth(process.env.jwt_sceret_key), notesController.addreminder);
+
+// delete the reminder
+router.delete('/:_id/reminder', userAuth(process.env.jwt_sceret_key), notesController.deletereminder);
+
+
+// update the reminder
+
+// router.put('/:_id/reminder', userAuth(process.jwt_sceret_key), notesController.updatereminder);
+
 
 // collabtor - object of email
 router.get('/:_id/collaborators', userAuth(process.env.jwt_sceret_key), notesController.getcollaborators);
